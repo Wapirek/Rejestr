305b6b6a64a700a99659d4ba80ec7941d836fdc6
Jedrelek <jedrelek@gmail.com>
2022-11-11 17:10:30 +0100

    add roles and admin

--- my-project/database/seeders/DatabaseSeeder.php
+++ my-project/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Middlewares\RoleOrPermissionMiddleware;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+        $this->call(RoleSeeder::class);
+        $this->call(AdminSeeder::class);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
